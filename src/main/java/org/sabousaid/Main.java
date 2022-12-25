7c65ba349293f15ac2302422f6545f056122cbb8
abousaid <johndoe@example.com>
2022-12-25 15:07:10 +0100

    JSON for Java

--- src/main/java/org/sabousaid/Main.java
+++ src/main/java/org/sabousaid/Main.java
@@ -1,12 +1,11 @@
 package org.sabousaid;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Objects;
 
 @SpringBootApplication
 /*
@@ -25,5 +24,34 @@ public class Main {
         return new GreetRecord("Hello World !!");
     }
 
-    record GreetRecord(String greet) {}
+    //record GreetRecord(String greet) {}
+
+    class GreetRecord {
+        private final String greet;
+
+        GreetRecord(String greet) {
+            this.greet = greet;
+        }
+
+        public String getGreet() {
+            return greet;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (!(o instanceof GreetRecord that)) return false;
+            return Objects.equals(getGreet(), that.getGreet());
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(getGreet());
+        }
+    }
 }
